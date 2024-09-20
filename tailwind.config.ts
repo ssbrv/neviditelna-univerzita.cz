ba4ba50ecf2e01eeb454135f0bfe334879e131de
Daniyar Sabirov <daniyar.sabirov@ami.cz>
2024-09-20 20:36:09 +0200

    init vue project

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./src/**/*.{js,ts,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+export default config;
