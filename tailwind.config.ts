3a154e35b0e104d95c27dfcbbe865813a2fe4c2a
Daniyar Sabirov <daniyar.sabirov@ami.cz>
2024-09-21 15:30:48 +0200

    override tailwind colors and shadows

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,23 @@ const config: Config = {
   prefix: "tw-",
   content: ["./src/**/*.{js,ts,vue}"],
   theme: {
-    extend: {},
+    colors: {
+      background: "rgba(var(--background))",
+      primary: "rgba(var(--primary))",
+      secondary: "rgba(var(--secondary))",
+
+      action: "rgba(var(--action))",
+      failure: "rgba(var(--failure))",
+      success: "rgba(var(--success))",
+
+      link: "rgba(var(--link))",
+      nav: {
+        focused: "rgba(var(--nav-focused))",
+      },
+    },
+    boxShadow: {
+      sm: "rgba(149, 157, 165, 0.2) 0px 8px 24px;",
+    },
   },
   plugins: [],
 };
